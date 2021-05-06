d613cb23f0845376ed5f63e7ba50b24b701e8853
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2021-05-06 10:56:28 +0100

    Vertex Buffer Layouts

--- Deya/include/Platform/OpenGL/OpenGlBuffer.h
+++ Deya/include/Platform/OpenGL/OpenGlBuffer.h
@@ -16,8 +16,12 @@ namespace Deya
 
         virtual void Bind() const override;
         virtual void Unbind() const override;
+
+        inline virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+        inline virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
     private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout;
     };
 
     /**
--- Deya/src/Deya/Application.cpp
+++ Deya/src/Deya/Application.cpp
@@ -14,6 +14,27 @@ namespace Deya
 
     Application* Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case Deya::ShaderDataType::Float:		return GL_FLOAT;
+            case Deya::ShaderDataType::Float2:		return GL_FLOAT;
+            case Deya::ShaderDataType::Float3:		return GL_FLOAT;
+            case Deya::ShaderDataType::Float4:		return GL_FLOAT;
+            case Deya::ShaderDataType::Int:			return GL_INT;
+            case Deya::ShaderDataType::Int2:		return GL_INT;
+            case Deya::ShaderDataType::Int3:		return GL_INT;
+            case Deya::ShaderDataType::Int4:		return GL_INT;
+            case Deya::ShaderDataType::Mat3:		return GL_FLOAT;
+            case Deya::ShaderDataType::Mat4:		return GL_FLOAT;
+            case Deya::ShaderDataType::Bool:		return GL_BOOL;
+        }
+
+		DY_CORE_ASSERT(false, "Unknown ShaderDataType");
+		return 0;
+    }
+
     Application::Application()
     {
         DY_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -28,17 +49,19 @@ namespace Deya
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
     
-        float vertices[6 * 3] = // 4 verts * 3 dimensions
+        // legume verts
+        float vertices[6 * 7] = // 4 verts * (3 dimensions + 4 colour channels)
         {
             // middle of legume
-            -0.125f,  0.3f, 0.0f, // top left
-            -0.125f, -0.3f, 0.0f, // bottom left
-             0.125f,  0.3f, 0.0f, // top right
-             0.125f, -0.3f, 0.0f, // bottom right
+			// POS						COLOUR
+            -0.125f,  0.3f, 0.0f,		0.25f, 0.13f, 0.03f, 0.0f, // top left
+            -0.125f, -0.3f, 0.0f, 		0.25f, 0.13f, 0.03f, 0.0f, // bottom left
+             0.125f,  0.3f, 0.0f,		0.25f, 0.13f, 0.03f, 0.0f, // top right
+             0.125f, -0.3f, 0.0f,		0.25f, 0.13f, 0.03f, 0.0f, // bottom right
 
             // top and bottom verts
-             0.0f,  0.45f, 0.0f, // top
-             0.0f, -0.45f, 0.0f  // bottom
+             0.0f,  0.45f, 0.0f,		0.35f, 0.23f, 0.05f, 0.0f, // top
+             0.0f, -0.45f, 0.0f,		0.15f, 0.03f, 0.01f, 0.0f  // bottom
         };
 
         uint32_t indices[12] = // the order to render our verts
@@ -51,8 +74,30 @@ namespace Deya
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr); // tell opengl what our data means
+        {
+			BufferLayout layout =
+			{
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+        uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout)
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer( 								//*Tell OpenGL what our data means
+				index,											// Index of attribute
+				element.GetComponentCount(),					// How many components there are in the attribute (vec2 = 2, vec3 = 3, ...)
+				ShaderDataTypeToOpenGLBaseType(element.Type), 	// The type of attribute this is (float, int, ...)
+				element.Normalized ? GL_TRUE : GL_FALSE, 		// Is the data normalized?
+				layout.GetStride(),								// The stride between the attributes
+				(const void*) element.Offset); 					// Offset between the start of the data, and the attribute			
+            index++;
+        }
 
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
@@ -60,12 +105,15 @@ namespace Deya
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
             out vec3 v_Position;
+			out vec4 v_Color;
 
             void main()
             {
                 v_Position = a_Position;
+				v_Color = a_Color;
                 gl_Position = vec4(a_Position, 1.0);
             }
         )";
@@ -76,11 +124,13 @@ namespace Deya
             layout(location = 0) out vec4 color;
 
             in vec3 v_Position;
+			in vec4 v_Color;
 
             void main()
             {
                 //color = vec4(0.31, 0.16, 0.15, 1.0); // BROWN (#4f2a26)
-                color = vec4(v_Position * 0.5 + 0.5, 1.0);
+                //color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
             }
         )";
 
